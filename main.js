c8d810a368eac529eb9e02ffe17c143c5adfdbd6
salvatore <tore0145@gmail.com>
2022-03-24 17:53:49 +0100

    finito?

---
+++ main.js
@@ -0,0 +1,20 @@
+let emailaccettate = ["matteo@gmail.com","fabio@gmail.com", "pietro@gmail.com","anna@gmail.com"];
+const email = prompt("inserisci la tua email");
+console.log(email);
+for (i = 0; i > emailaccettate.length; i++) {
+    if (email = emailaccettate[i]); {
+    const numerorandomdelcomputer = Math.floor(Math.random()*6 +1);
+    const scelta1 = parseInt(numerorandomdelcomputer);
+    const numerorandondelgiocatore = math.floor(math.random()*6+1);
+    const scelta2 = parseInt(numerorandondelgiocatore);
+
+} else if(numerorandomdelcomputer>numerorandomdelcomputer){
+    alert('ha vinto il computer ');
+} else if(numerorandondelgiocatore>numerorandomdelcomputer){
+    alert('hai vinto tu');
+}
+
+
+
+}
+alert('email non valida');
